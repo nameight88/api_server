c5321d4f0c835f7e15c0ae607843dc8bbc4a4e7c
nam david <161475976+nameight88@users.noreply.github.com>
2025-12-04 06:22:28 +0900

    Update database.js

--- src/API_Server/src/config/database.js
+++ src/API_Server/src/config/database.js
@@ -1,12 +1,12 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  process.env.COUBOX_DB_DATABASE || 'couboxdb',
-  process.env.COUBOX_DB_USER || 'root', 
-  process.env.COUBOX_DB_PASSWORD || '',
+  process.env.DB_DATABASE || 'userdb',
+  process.env.DB_USER || 'scout', 
+  process.env.ASSWORD || 'tiger',
   {
-    host: process.env.COUBOX_DB_HOST || '61.32.97.138',
-    port: process.env.DB_PORT || 33306,
+    host: process.env.COUBOX_DB_HOST || '127.0.0.1',
+    port: process.env.DB_PORT || 3306,
     dialect: 'mysql',
     logging: process.env.NODE_ENV === 'development' ? console.log : false,
     timezone: '+09:00',
